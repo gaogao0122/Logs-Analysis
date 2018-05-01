4c2f4cb907b6e1371183dae260e4b2d5eedf9fe3
Yun Gao <gaoyun122@gmail.com>
2018-05-01 16:54:58 -0400

    correct Q2 sql, add shebang line

--- report_tool.py
+++ report_tool.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import psycopg2
 
 
@@ -6,24 +7,28 @@ def main():
     cursor = db.cursor()
     # 1. What are the most popular three articles of all time
     cursor.execute("select A.title, L.views from articles as A," +
-                   " (select replace(trim('/article/' from path), '-', ' ') " +
+                   " (select replace(replace(log.path, " +
+                   "'/article/', ''), '-', ' ') " +
                    "as name, count(path) as views from log " +
                    "where path != '/' group by path " +
                    "order by views desc limit 3) as L " +
                    "where A.title ilike concat('%', L.name, '%') " +
-                   "order by L.views desc;")
+                   "order by L.views desc")
     result = cursor.fetchall()
     print("-----------------------Question 1---------------------------")
     print("1. What are the most popular three articles of all time?\n")
     for tuple in result:
         print (tuple[0] + " -- " + str(tuple[1]) + " views")
     # 2. Who are the most popular article authors of all time?
-    cursor.execute("select list.name, sum(L.times) as views from list, " +
-                   "(select replace(trim('/article/' from path), '-', ' ') " +
-                   "as title, count(path) as times from log " +
-                   "where path != '/' group by path) as L " +
-                   "where list.title ilike concat('%', L.title, '%') " +
-                   "group by list.name order by views desc;")
+    cursor.execute("select A.name, sum(L.views) as times from " +
+                   "(select articles.title, authors.name from articles, " +
+                   "authors where articles.author = authors.id) as A, " +
+                   "(select replace(replace(log.path, " +
+                   "'/article/', ''),'-',' ') " +
+                   "as title, count(log.path) as views " +
+                   "from log group by path) as L " +
+                   "where A.title ilike concat('%', L.title, '%') " +
+                   "group by A.name order by times desc")
     result = cursor.fetchall()
     print("\n-----------------------Question 2---------------------------")
     print("2. Who are the most popular article authors of all time?\n")
@@ -41,10 +46,12 @@ def main():
                    "(select to_char(time, 'Mon DD, YYYY') as date, " +
                    "count(status) as total_status from log " +
                    "group by date) as T where E.date = T.date) as F " +
-                   "order by error_percentage desc limit 1;")
+                   "where round(F.error_status::numeric/" +
+                   "F.total_status::numeric*100,1) > 1 " +
+                   "order by error_percentage desc limit 1")
     result = cursor.fetchall()
     print("\n-----------------------Question 3---------------------------")
-    print("On which days did more than 1% of requests lead to errors?\n")
+    print("3. On which days did more than 1% of requests lead to errors?\n")
     for tuple in result:
         print (tuple[0] + " -- " + str(tuple[1]) + "%")
     db.close()
